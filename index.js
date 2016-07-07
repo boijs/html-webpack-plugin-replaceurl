84febdf8c658612dbb13c5379c2f466fe575bd47
zhoujunpeng <zjp0432@163.com>
2016-07-07 17:55:04 +0800

    修复不适用hash指纹识别错误问题

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 'use strict';
 let fs = require('fs');
 let path = require('path');
+let _ = require('lodash');
 // 默认配置
 // mainFilePrefix: 文件名前缀，用来正则匹配待替换文件名
 // vendor：如果需要插入vendor文件请务必配置此项，value是需插入的文件名，与webpack配置保持一致
@@ -36,30 +37,40 @@ HtmlWebpackReplaceurlPlugin.prototype.replaceUrl = function(compilation, htmlWeb
 
     let _html = htmlPluginData.html;
     let _assets = htmlPluginData.assets;
+    let _chunks = Object.keys(_assets.chunks);
 
     // 替换js url
     for (let i = 0, len = _assets.js.length; i < len; i++) {
         let jsFile = _assets.js[i];
-        let _arr = jsFile.split('/');
-        let _filename = _arr[_arr.length - 1];
+        let _filename = _.takeRight(jsFile.split('/'))[0];
         let _regJsSrc = null;
         if (REG_JS_FILENAME.test(jsFile)) {
-            let _originName = _filename.split(/\.[a-z0-9]+\.js$/.exec(_filename))[0] + '.js';
-            _regJsSrc = new RegExp('src\s?=\s?(\'|\")'+'[\.A-Za-z0-9_\/]*' + _originName.split('.').join('\\.')+'(\'|\")', 'g');
+            let _originName = '';
+            if(_.indexOf(_chunks,_filename.split(/\.js$/)[0]) !== -1){
+                _originName = _filename;
+            }else{
+                _originName = _filename.split(/\.[a-z0-9]+\.js$/.exec(_filename))[0] + '.js';
+            }
+            _regJsSrc = new RegExp('src\\s?=\\s?(\'|\")'+'[\.A-Za-z0-9_\/]*' + _originName.split('.').join('\\.')+'(\'|\")', 'g');
         }
+
         _html = _html.replace(_regJsSrc, 'src = \"'+jsFile+'\"');
     }
     // 替换css url
     for (let i = 0, len = _assets.css.length; i < len; i++) {
         let cssFile = _assets.css[i];
-        let _arr = cssFile.split('/');
-        let _filename = _arr[_arr.length - 1];
+        let _filename = _.takeRight(cssFile.split('/'))[0];
         let _regCssSrc = null;
         if (REG_CSS_FILENAME.test(cssFile)) {
-            let _originName = _filename.split(/\.[a-z0-9]+\.css$/.exec(_filename))[0] + '.css';
-            _regCssSrc = new RegExp('href\s?=\s?(\'|\")'+'[\.A-Za-z0-9_\/]*' + _originName.split('.').join('\\.')+'(\'|\")', 'g');
+            let _originName = '';
+            if(_.indexOf(_chunks,_filename.split(/\.css$/)[0]) !== -1){
+                _originName = _filename;
+            }else{
+                _originName = _filename.split(/\.[a-z0-9]+\.css$/.exec(_filename))[0] + '.css';
+            }
+            _regCssSrc = new RegExp('href\\s?=\\s?(\'|\")'+'[\.A-Za-z0-9_\/]*' + _originName.split('.').join('\\.')+'(\'|\")', 'g');
         } else {
-            _regCssSrc = new RegExp('href\s?=\s?(\'|\")'+'[\.A-Za-z0-9_\/]*' + _filename.split('.').join('\\.')+'(\'|\")', 'g');
+            _regCssSrc = new RegExp('href\\s?=\\s?(\'|\")'+'[\.A-Za-z0-9_\/]*' + _filename.split('.').join('\\.')+'(\'|\")', 'g');
         }
         _html = _html.replace(_regCssSrc, 'href = \"'+cssFile+'\"');
     }
