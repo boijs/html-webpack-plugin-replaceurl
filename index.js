34402fc5e46fbcc3af40963690e35e9bdd3528d7
zhoujunpeng <zjp0432@163.com>
2016-09-01 17:50:19 +0800

    细化配置项&支持多种缓存方案

--- index.js
+++ index.js
@@ -1,77 +1,89 @@
 /**
- * 替换html中静态资源的url
+ * @desc 替换html中静态资源的url
  */
 'use strict';
+
 let fs = require('fs');
 let path = require('path');
-// 默认配置
-// mainFilePrefix: 文件名前缀，用来正则匹配待替换文件名
-// vendor：如果需要插入vendor文件请务必配置此项，value是需插入的文件名，与webpack配置保持一致
+
+/**
+ * @desc 默认配置
+ * @type {Object}
+ */
 const DEFAULT_OPTIONS = {
-    mainFilePrefix: {
-        js: 'main',
-        css: 'main'
-    },
-    vendor: 'vendor.js'
+  js: {
+    // 文件名前缀，用来正则匹配待替换文件名
+    mainFilePrefix: 'main',
+    // 代替换文件名是否包含hash指纹
+    useHash: false
+  },
+  style: {
+    mainFilePrefix: 'main',
+    useHash: false
+  },
+  vendor: 'vendor.js',
+  // 是否在url末尾加入时间戳，默认false
+  urlTimestamp: false
 };
 
 let HtmlWebpackReplaceurlPlugin = function(options) {
-    this.options = Object.assign({}, DEFAULT_OPTIONS, options);
+  this.options = Object.assign({}, DEFAULT_OPTIONS, options);
 }
 
 HtmlWebpackReplaceurlPlugin.prototype.apply = function(compiler) {
-    let _this = this;
-    compiler.plugin('compilation', function(compilation) {
-        compilation.plugin('html-webpack-plugin-before-html-processing', function(htmlPluginData, callback) {
-            _this.replaceUrl(compilation, htmlPluginData.plugin.options, htmlPluginData, callback);
-        });
+  let _this = this;
+  compiler.plugin('compilation', function(compilation) {
+    compilation.plugin('html-webpack-plugin-before-html-processing', function(htmlPluginData, callback) {
+      _this.replaceUrl(compilation, htmlPluginData.plugin.options, htmlPluginData, callback);
     });
+  });
 }
 
 HtmlWebpackReplaceurlPlugin.prototype.replaceUrl = function(compilation, htmlWebpackPluginOptions, htmlPluginData,
-    callback) {
-    let _this = this;
-    const REG_JS_FILENAME = new RegExp(_this.options.mainFilePrefix.js + '[\\.\\w+]+\\.js');
-    const REG_CSS_FILENAME = new RegExp(_this.options.mainFilePrefix.css + '[\\.\\w+]+\\.css');
+  callback) {
+  let _this = this;
+  const REG_JS_FILENAME = new RegExp(_this.options.js.mainFilePrefix + '[\\.\\w+]+\\.js');
+  const REG_CSS_FILENAME = new RegExp(_this.options.style.mainFilePrefix + '[\\.\\w+]+\\.css');
 
-    let _html = htmlPluginData.html;
-    let _assets = htmlPluginData.assets;
+  let _html = htmlPluginData.html;
+  let _assets = htmlPluginData.assets;
 
-    // 替换js url
-    for (let i = 0, len = _assets.js.length; i < len; i++) {
-        let jsFile = _assets.js[i];
-        let _arr = jsFile.split('/');
-        let _filename = _arr[_arr.length - 1];
-        let _regJsSrc = null;
-        if (REG_JS_FILENAME.test(jsFile)) {
-            let _originName = _filename.split(/\.[a-z0-9]+\.js$/.exec(_filename))[0] + '.js';
-            _regJsSrc = new RegExp('src\s?=\s?(\'|\")'+'[\.A-Za-z0-9_\/]*' + _originName.split('.').join('\\.')+'(\'|\")', 'g');
-        }
-        _html = _html.replace(_regJsSrc, 'src = \"'+jsFile+'\"');
-    }
-    // 替换css url
-    for (let i = 0, len = _assets.css.length; i < len; i++) {
-        let cssFile = _assets.css[i];
-        let _arr = cssFile.split('/');
-        let _filename = _arr[_arr.length - 1];
-        let _regCssSrc = null;
-        if (REG_CSS_FILENAME.test(cssFile)) {
-            let _originName = _filename.split(/\.[a-z0-9]+\.css$/.exec(_filename))[0] + '.css';
-            _regCssSrc = new RegExp('href\s?=\s?(\'|\")'+'[\.A-Za-z0-9_\/]*' + _originName.split('.').join('\\.')+'(\'|\")', 'g');
-        } else {
-            _regCssSrc = new RegExp('href\s?=\s?(\'|\")'+'[\.A-Za-z0-9_\/]*' + _filename.split('.').join('\\.')+'(\'|\")', 'g');
-        }
-        _html = _html.replace(_regCssSrc, 'href = \"'+cssFile+'\"');
-    }
-    // 插入vendor文件
-    // @todo 目前需要手动在html中写入vendor文件，后续会研究如何自动插入
-    if (_this.options.vendor && _assets.chunks.hasOwnProperty('vendor')) {
-        let _regJsSrc = new RegExp('[\.A-Za-z0-9_\/]+' + _this.options.vendor.split('.').join('\\.'), 'g');
-        _html = _html.replace(_regJsSrc, _assets.chunks.vendor.entry);
-    }
-    htmlPluginData.html = _html;
+  // 生成时间戳，false时为null
+  let urlTimestamp = _this.options.urlTimestamp && (new Date()).getTime() || null;
 
-    callback(null, htmlPluginData);
+  // 替换js url
+  for (let i = 0, len = _assets.js.length; i < len; i++) {
+    let jsFile = _assets.js[i];
+    let _arr = jsFile.split('/');
+    let _filename = _arr[_arr.length - 1];
+    let _regJsSrc = null;
+    if (REG_JS_FILENAME.test(jsFile)) {
+      let _originName = _this.options.js.useHash ? _filename.split(/[a-z\d]+\.js$/.exec(_filename))[0] + 'js' : _filename;
+      _regJsSrc = new RegExp('src\\s?=\\s?(\'|\")' + '[\\.\\w\/]*' + _originName.split('.').join('\\.') + '(\'|\")', 'g');
+      _html = _html.replace(_regJsSrc, 'src = \"' + (urlTimestamp ? jsFile + '?' + urlTimestamp : jsFile) + '\"');
+    }
+  }
+  // 替换css url
+  for (let i = 0, len = _assets.css.length; i < len; i++) {
+    let cssFile = _assets.css[i];
+    let _arr = cssFile.split('/');
+    let _filename = _arr[_arr.length - 1];
+    let _regCssSrc = null;
+    if (REG_CSS_FILENAME.test(cssFile)) {
+      let _originName = _this.options.style.useHash ? _filename.split(/[a-z\d]+\.css$/.exec(_filename))[0] + 'css' : _filename;
+      _regCssSrc = new RegExp('href\\s?=\\s?(\'|\")' + '[\.\\w\/]*' + _originName.split('.').join('\\.') + '(\'|\")', 'g');
+      _html = _html.replace(_regCssSrc, 'href = \"' + (urlTimestamp ? cssFile + '?' + urlTimestamp : cssFile) + '\"');
+    }
+  }
+  // 插入vendor文件
+  // @todo 目前需要手动在html中写入vendor文件，后续会研究如何自动插入
+  if (_this.options.vendor && _assets.chunks.hasOwnProperty('vendor')) {
+    let _regJsSrc = new RegExp('[\.\w\/]+' + _this.options.vendor.split('.').join('\\.'), 'g');
+    _html = _html.replace(_regJsSrc, _assets.chunks.vendor.entry);
+  }
+  htmlPluginData.html = _html;
+
+  callback(null, htmlPluginData);
 }
 
 module.exports = HtmlWebpackReplaceurlPlugin;
